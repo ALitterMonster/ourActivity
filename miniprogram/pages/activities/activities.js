70392c711e09a483f5bb32394a9960fe71e6e90a
du_ruolin@hotmail.com <du_ruolin@hotmail.com>
2019-02-12 00:33:44 +0800

    V1.0

---
+++ miniprogram/pages/activities/activities.js
@@ -0,0 +1,84 @@
+// miniprogram/pages/activities/activities.js
+
+const db = wx.cloud.database() 
+
+Page({
+  
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    activities: [],
+    openid:'o2qXq0GG2vJ0FWd2T0PjsYD42hVI'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    db.collection('activity').get({
+      success: res => {
+        this.setData({
+          activities: res.data
+        })
+        console.log('[数据库] [查询记录] 成功: ', res)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
